8092e4d2ae333b8fefbd8ac96dbf066ed8b69fc6
ma7madAshraf <ma7mad.ashraf@gmail.com>
2024-10-12 20:05:24 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
     <div className="grid gap-24 lg:grid-cols-2 items-center mt-16">
       <div>
         <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-5xl">
-          We adore technology. <br />
-          We are passionate about innovation.
+          Transforming Spaces with Elegant and Modern Furniture for Every Room
         </h1>
         <p className="mt-8 text-xl max-w-xl leading-8">
-          We in Mo store adore technology adipisicing elit. Iusto voluptatibus
-          nihil ea earum omnis, eius eveniet architecto a ullam sapiente.
+          Discover stylish, high-quality furniture that brings comfort and
+          elegance to every room. Create the perfect space with our curated
+          collections
         </p>
         <div className="mt-10 ">
           <Link to="products" className="btn btn-primary ">
